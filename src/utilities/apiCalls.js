aa73c029c77346dc8c1c95fec9b082d090c5dd3e
Bobby Vasquez <shootbv@gmail.com>
2021-07-25 14:57:19 -0600

    Add basic POST method from Form.js to apiCalls.js

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -5,6 +5,17 @@ const getQuestions = () => {
   .then(checkResponse)
 }
 
+const addGame = (pastGame) => {
+  return fetch(url + '/past-games', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(pastGame)
+  })
+  .then(checkResponse)
+}
+
 const checkResponse = (response) => {
   if (response.ok) {
     return response.json();
@@ -12,3 +23,5 @@ const checkResponse = (response) => {
     throw new Error(`Request could not go through.`);
   }
 };
+
+export { getQuestions, addGame }
