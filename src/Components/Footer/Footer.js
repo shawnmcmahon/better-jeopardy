057ad4a136ca546c9f6bb27970d92ed8c7077a1a
Bobby Vasquez <shootbv@gmail.com>
2021-07-26 13:24:50 -0600

    Add links as buttons to footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,13 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-
+    <footer>
+      <ul className="gh-links">
+        <li><a href="https://github.com/hoomberto"><button>Bobby Vasquez</button></a></li>
+        <li><a href="https://github.com/novaraptur"><button>Dean Cook</button></a></li>
+        <li><a href="https://github.com/shawnmcmahon"><button>Shawn McMahon</button></a></li>
+      </ul>
+    </footer>
   )
 }
 
