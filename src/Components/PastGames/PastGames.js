c807bb76a338c7327643d2d98e771b41d6443748
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-07-31 18:16:22 -0600

    Map now renders one past game without info to page

--- src/Components/PastGames/PastGames.js
+++ src/Components/PastGames/PastGames.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './PastGames.css';
 import { getPastGames } from '../../utilities/apiCalls'
+import SavedGame from '../SavedGame/SavedGame';
+import Tile from '../Tile/Tile';
 
 const PastGames = () => {
 
@@ -9,27 +11,38 @@ const PastGames = () => {
 
   useEffect(() => {
     getPastGames()
-      .then((data) => setPastGames(data))
+      .then((data) => {
+        setPastGames(data)
+        // console.log('data', data)
+      })
+      .catch((err) => console.log(err))
   }, [])
 
-  console.log('hi', pastGameData.pastGames);
-
-  const renderPastGames = () => {
-    return pastGameData.pastGames.map((gameData) => {
-      <article className="past-game-card">
-        <h3 className="date">{gameData.date}</h3>\
-        <h3 className="id">{gameData.game_id}</h3>
-        <h3 className="questions">{gameData.questions}</h3>
-        <h3 className="score">{gameData.score}</h3>
-      </article>
+  let renderPastGames
+  if (!!pastGameData.pastGames) {
+    const theDataWeNeed = [pastGameData.pastGames];
+    console.log('hi', theDataWeNeed);
+    renderPastGames = theDataWeNeed.map((gameData) => {
+      return (
+      <SavedGame
+        date={gameData.date}
+        key={gameData.game_id}
+        id={gameData.game_id}
+        questions={gameData.questions}
+        score={gameData.score}
+        />    
+      )
     })
+    return renderPastGames;
   }
+  
+
 
   return (
     <section className="past-games-section">
       <Link to="/"><button className="nav-button">Back</button></Link>
       <h2>Past Games</h2>
-      {renderPastGames}
+      {!!pastGameData.length && <div>{renderPastGames}</div>}
     </section>
   )
 }
---
+++ src/Components/SavedGame/SavedGame.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SavedGame.css';
+
+
+const SavedGame = ({date, id, questions, score}) => {
+  return (
+    <div className="saved-game-wrapper"> 
+      <article> 
+        <h2 className="date">{date}</h2> 
+        <h2 className="id">{id}</h2>
+        <h2 className="questions">{questions}</h2>
+        <h2 className="score">{score}</h2>
+      </article>
+    </div>
+  )
+}
+
+export default SavedGame;
