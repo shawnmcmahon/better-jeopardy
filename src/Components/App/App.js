8751029bba7d30e2b593c8d426e55996967048b4
Bobby Vasquez <shootbv@gmail.com>
2021-08-01 10:38:06 -0600

    Add functionality to add Player name to state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import { BrowserRouter } from 'react-router-dom'
 import Header from '../Header/Header';
 import Game from '../Game/Game';
+import Form from '../Form/Form'
 
 const App = () => {
+  // const [playerName, setPlayerName] = useState('');
+
   return (
   <BrowserRouter>
     <main>
       <Header />
+      
       <Game />
     </main>
   </BrowserRouter>
