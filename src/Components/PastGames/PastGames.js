ab7e8f6ec00144f3c2db8bee314580a262ba36e0
Bobby Vasquez <shootbv@gmail.com>
2021-07-31 23:03:54 -0600

    Display Past games on page, add styling to past games

--- src/Components/PastGames/PastGames.js
+++ src/Components/PastGames/PastGames.js
@@ -11,51 +11,65 @@ const PastGames = () => {
 
   useEffect(() => {
     getPastGames()
-      .then((data) => {
+      .then(data => {
+        console.log('WHAT WE WANT', data.pastGames)
         setPastGames(data.pastGames)
-        // console.log('data', data)
       })
       .catch((err) => console.log(err))
   }, [])
 
-  let renderPastGames
-  if (!!pastGameData) {
-    const theDataWeNeed = pastGameData
-    
-    console.log('hi', theDataWeNeed);
-    return renderPastGames = theDataWeNeed.map((gameData, index) => {
-      console.log('mapData', gameData)
-      const {date, game_id, questions, score} = gameData;
+  const renderPastGames = () => {
+    return pastGameData.map(data => {
+      const { game_id, date, questions, score } = data;
+      console.log('id', game_id)
+      console.log('date', date)
+      console.log('questions', questions)
+      console.log('score', score)
       return (
-      <div>
         <SavedGame
-            date={date}
-            key={game_id}
-            id={game_id}
-            questions={questions}
-            score={score}
-          /> 
-          {/* <SavedGame
-            date={gameData[index].date}
-            key={gameData[index].game_id}
-            id={gameData[index].game_id}
-            questions={gameData[index].questions}
-            score={gameData[index].score}
-          />  */}
-      </div> 
-        
+          date={date}
+          key={game_id}
+          questions={questions}
+          score={score}
+        />
       )
     })
-    // return renderPastGames;
   }
-  
-
+  // if (!!pastGameData.length) {
+  //   const theDataWeNeed = pastGameData
+  //
+  //   console.log('hi', theDataWeNeed);
+  //   return renderPastGames = theDataWeNeed.map((gameData, index) => {
+  //     console.log('mapData', gameData)
+  //     const {date, game_id, questions, score} = gameData;
+  //     return (
+  //     <div>
+  //       <SavedGame
+  //           date={date}
+  //           key={game_id}
+  //           id={game_id}
+  //           questions={questions}
+  //           score={score}
+  //         />
+  //         {/* <SavedGame
+  //           date={gameData[index].date}
+  //           key={gameData[index].game_id}
+  //           id={gameData[index].game_id}
+  //           questions={gameData[index].questions}
+  //           score={gameData[index].score}
+  //         />  */}
+  //     </div>
+  //
+  //     )
+  //   })
+  //   // return renderPastGames;
+  // }
 
   return (
     <section className="past-games-section">
       <Link to="/"><button className="nav-button">Back</button></Link>
       <h2>Past Games</h2>
-      {!!pastGameData.length && {renderPastGames}}
+      {pastGameData.length ? <div>{renderPastGames()}</div> : <h3>Loading...</h3>}
     </section>
   )
 }
--- src/Components/SavedGame/SavedGame.js
+++ src/Components/SavedGame/SavedGame.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './SavedGame.css';
+import dayjs from 'dayjs'
 
 
 const SavedGame = ({date, id, questions, score}) => {
+
+  const questionContainer = questions.map(question => {
+    return (
+
+        <li>{question.question} | {question.answered_correct ? "Answered Correct" : "Incorrect Answer"}</li>
+
+    )
+  })
+
   return (
-    <div className="saved-game-wrapper"> 
-      <article> 
-        <h2 className="id">{id}</h2>
-        <h2 className="questions">{questions}</h2>
-        <h2 className="date">{date}</h2> 
-        <h2 className="score">{score}</h2>
+    <div className="saved-game-wrapper">
+      <article className="saved-game">
+        <h2 className="date">Played on: {dayjs(date).format("MM/DD/YYYY")}</h2>
+        <h2 className="score">Score: ${score}</h2>
+        <ul className="questions">{questionContainer}</ul>
       </article>
     </div>
   )
