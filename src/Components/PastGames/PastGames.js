689b46fa9d5a9375147767294669780dab1f932d
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-08-02 15:47:13 -0600

    Remove unused code and console logs

--- src/Components/PastGames/PastGames.js
+++ src/Components/PastGames/PastGames.js
@@ -14,7 +14,6 @@ const PastGames = () => {
     getPastGames()
       .then(data => {
         setPastGames(data.pastGames)
-        console.log(pastGameData)
       })
       .catch((err) => console.log(err))
   }, [])
@@ -33,35 +32,7 @@ const PastGames = () => {
       )
     })
   }
-  // if (!!pastGameData.length) {
-  //   const theDataWeNeed = pastGameData
-  //
-  //   console.log('hi', theDataWeNeed);
-  //   return renderPastGames = theDataWeNeed.map((gameData, index) => {
-  //     console.log('mapData', gameData)
-  //     const {date, game_id, questions, score} = gameData;
-  //     return (
-  //     <div>
-  //       <SavedGame
-  //           date={date}
-  //           key={game_id}
-  //           id={game_id}
-  //           questions={questions}
-  //           score={score}
-  //         />
-  //         {/* <SavedGame
-  //           date={gameData[index].date}
-  //           key={gameData[index].game_id}
-  //           id={gameData[index].game_id}
-  //           questions={gameData[index].questions}
-  //           score={gameData[index].score}
-  //         />  */}
-  //     </div>
-  //
-  //     )
-  //   })
-  //   // return renderPastGames;
-  // }
+  
 
   return (
     <section className="past-games-section">
